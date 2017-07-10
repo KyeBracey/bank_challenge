db1fecd1a0cba81096acdc7fb4c6346e254f3f1b
Kynosaur <kye.bracey@gmail.com>
2017-07-10 16:49:08 +0100

    Adds StatementPrinter class with print_statement method

---
+++ lib/statement_printer.rb
@@ -0,0 +1,5 @@
+class StatementPrinter
+  def print_statement(transactions = [])
+    puts 'No transaction history' if transactions == []
+  end
+end
---
+++ spec/statement_printer_spec.rb
@@ -0,0 +1,11 @@
+require 'statement_printer'
+
+describe StatementPrinter do
+  subject(:statementPrinter) { described_class.new }
+  describe '#print_statement' do
+    it 'Prints out "No transaction history" when passed an empty array' do
+      expect(STDOUT).to receive(:puts).with('No transaction history')
+      statementPrinter.print_statement
+    end
+  end
+end
