fd722ab7edf6e823afd78ba30b0108a8f1aa2cf3
Kynosaur <kye.bracey@gmail.com>
2017-07-10 16:51:22 +0100

    Adds header to print_statement method

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,5 +1,6 @@
 class StatementPrinter
   def print_statement(transactions = [])
+    puts 'date || credit || debit || balance'
     puts 'No transaction history' if transactions == []
   end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -4,6 +4,7 @@ describe StatementPrinter do
   subject(:statementPrinter) { described_class.new }
   describe '#print_statement' do
     it 'Prints out "No transaction history" when passed an empty array' do
+      expect(STDOUT).to receive(:puts).with('date || credit || debit || balance')
       expect(STDOUT).to receive(:puts).with('No transaction history')
       statementPrinter.print_statement
     end
